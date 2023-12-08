b78f633883c68dddb8f82d374463d7dd3a63d424
Elijah Jose Arcedera <elijaha@mosaic-solutions.com>
2023-12-09 04:29:58 +0800

    Adjusts header width

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,23 +3,29 @@ import { UserButton } from "@clerk/nextjs";
 import { SignedIn, useUser } from "@clerk/clerk-react";
 import React from "react";
 
+const copy = {
+  title: "ez finance tracker",
+};
+
 const Header = () => {
   const { isLoaded } = useUser();
   return (
-    <nav className="bg-base-200 sticky top-0 flex w-full items-center justify-between py-2 shadow-sm shadow-neutral-700/10">
-      <div className="flex">
-        <a className="btn btn-ghost text-primary text-xl">ez finance tracker</a>
-      </div>
-      <div className="container mx-auto">
-        <div className="flex w-full flex-wrap items-center justify-end">
-          <div className="pr-5">
-            {!isLoaded ? (
-              <span className="loading loading-spinner loading-md mr-1 mt-1"></span>
-            ) : (
-              <SignedIn>
-                <UserButton afterSignOutUrl="/" />
-              </SignedIn>
-            )}
+    <nav className="bg-base-200 sticky top-0 flex w-full flex-row items-center justify-evenly py-2 shadow-sm shadow-neutral-700/10">
+      <div className="container max-w-2xl flex-row">
+        <div className="flex">
+          <a className="btn btn-ghost text-primary text-xl">{copy.title}</a>
+          <div className="container mx-auto my-auto">
+            <div className="flex w-full flex-wrap items-center justify-end">
+              <div className="pr-5">
+                {!isLoaded ? (
+                  <span className="loading loading-spinner loading-md mr-1 mt-1"></span>
+                ) : (
+                  <SignedIn>
+                    <UserButton afterSignOutUrl="/" />
+                  </SignedIn>
+                )}
+              </div>
+            </div>
           </div>
         </div>
       </div>
