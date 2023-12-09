3f6168063c04ce870c01f0933f116d522ecec110
Elijah Jose Arcedera <elijaha@mosaic-solutions.com>
2023-12-10 04:38:56 +0800

    Refactors render

--- src/app/components/pinButtonLogin.tsx
+++ src/app/components/pinButtonLogin.tsx
@@ -85,41 +85,48 @@ const PinButtonLogin: React.FC<PinButtonLoginProps> = ({
     ));
   };
 
-  return attemptsLeft > 0 ? (
+  return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold">{copy.secretLogin}</h1>
-      <div className="text-ghost text-sm">
-        {hasAttemptedIncorrectly
-          ? `${copy.attemptsLeft(attemptsLeft)}`
-          : copy.enterFourDigits}
-      </div>
-      <div className="mb-4 mt-2">
-        <input
-          type="password"
-          value={pin}
-          disabled
-          className={`max-w-xs rounded border border-gray-300 p-2 text-center ${
-            isIncorrect ? "shake-animation" : ""
-          }`}
-        />
-      </div>
-      {generateButtons()}
-    </div>
-  ) : (
-    <>
       <Image
         loading="eager"
         src="/easter-egg.gif"
         width={640}
         height={640}
         alt="Easter Egg"
+        className={attemptsLeft > 0 ? "hidden" : "block"}
       />
-      <div className="mt-1">
-        <h1 className="blinking-text-3 text-2xl font-bold">{copy.thankYou}</h1>
-        <h1 className="blinking-text-2 text-2xl font-bold">{copy.thankYou}</h1>
-        <h1 className="blinking-text text-2xl font-bold">{copy.thankYou}</h1>
-      </div>
-    </>
+      {attemptsLeft <= 0 ? (
+        <div className="mt-1">
+          <h1 className="blinking-text-3 text-2xl font-bold">
+            {copy.thankYou}
+          </h1>
+          <h1 className="blinking-text-2 text-2xl font-bold">
+            {copy.thankYou}
+          </h1>
+          <h1 className="blinking-text text-2xl font-bold">{copy.thankYou}</h1>
+        </div>
+      ) : (
+        <>
+          <h1 className="text-2xl font-bold">{copy.secretLogin}</h1>
+          <div className="text-ghost text-sm">
+            {hasAttemptedIncorrectly
+              ? `${copy.attemptsLeft(attemptsLeft)}`
+              : copy.enterFourDigits}
+          </div>
+          <div className="mb-4 mt-2">
+            <input
+              type="password"
+              value={pin}
+              disabled
+              className={`max-w-xs rounded border border-gray-300 p-2 text-center ${
+                isIncorrect ? "shake-animation" : ""
+              }`}
+            />
+          </div>
+          {generateButtons()}
+        </>
+      )}
+    </div>
   );
 };
 
